b8c537bfb35ecfac922a43bc10a5b20fb93cec84
ozgenselin@gmail.com <ozgenselin@gmail.com>
2024-04-16 22:22:52 +0000

    file created for intermediate analyses margin is added

---
+++ models/intermediate/int_sales_margin.sql
@@ -0,0 +1,17 @@
+-- This query calculates the margin 
+-- margin = revenue - purchase_cost
+-- purchase_cost - quantity * purchase_price
+
+
+SELECT products_id
+       ,date_date
+       ,orders_id
+       ,revenue
+       ,quantity
+       ,CAST(purchase_price AS FLOAT64)
+       ,ROUND(sales.quantity*CAST(products.purchase_price AS FLOAT64),2) AS purchase_cost
+       ,ROUND(sales.revenue - sales.quantity*CAST(products.purchase_price AS FLOAT64), 2) AS margin
+FROM {{ref("stg_raw__sales")}} sales
+LEFT JOIN {{ref("stg_raw__products")}} products
+    USING (products_id)
+
