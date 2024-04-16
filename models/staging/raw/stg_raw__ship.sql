1a9f511437361345197f95dc2554b4c7561993c9
ozgenselin@gmail.com <ozgenselin@gmail.com>
2024-04-16 19:11:02 +0000

    Cleaned the raw data by removing duplicate in ship and fixing the column name in sales

--- models/staging/raw/stg_raw__ship.sql
+++ models/staging/raw/stg_raw__ship.sql
@@ -11,12 +11,11 @@ renamed as (
     select
         orders_id,
         shipping_fee,
-        shipping_fee_1,
         logcost,
         ship_cost
 
     from source
-
+   
 )
 
 select * from renamed
